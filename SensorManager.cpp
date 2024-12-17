eb824c92c73e1f84ead8a087ddd63cb25f67a0ff
JG <simbachu@gmail.com>
2024-12-17 09:10:09 +0100

    Commented the implementations

--- SensorManager.cpp
+++ SensorManager.cpp
@@ -3,6 +3,11 @@
 #include <random>
 #include "SensorManager.h"
 
+/*
+    Namespace for different source functions for the data.
+    You could make one that consumes a list for instance.
+*/
+
 namespace sensor_reading{
     float random(float low, float high, std::mt19937 &gen){
         std::uniform_real_distribution<float> interval (low, high);
@@ -10,9 +15,17 @@ namespace sensor_reading{
     }
 }
 
+/*
+    Virtual destructor for subclassing with smart pointers
+*/
+
 Sensor::~Sensor(){
 
 }
+
+/*
+    Functor for threading
+*/
 
 void TempSensor::operator()() {
     std::mt19937 mt (std::random_device{}());
@@ -23,15 +36,27 @@ void TempSensor::operator()() {
     }
 }
 
+/*
+    For reading sensor value
+*/
 
 float TempSensor::poll() const {
     return last_measurement;
 }
 
+/*
+    Constructor is non-trivial and requires defining the sensors
+    and the threads we are using for them
+*/
+
 SensorManager::SensorManager(){
     temp_sensor = std::make_unique<TempSensor>();
     sensor_threads.push_back(std::thread(std::ref(*temp_sensor)));
 }
+
+/*
+    Joining the threads in the destructor like a jthread
+*/
 
 SensorManager::~SensorManager(){
     for (auto& thread : sensor_threads){
@@ -39,9 +64,17 @@ SensorManager::~SensorManager(){
     }
 }
 
+/*
+    Functor for threading
+*/
+
 void SensorManager::operator()(std::vector<WeatherReport> *out, std::mutex *lock_out){
     report(out, lock_out);
 }
+
+/*
+    Could probably move all this into the functor?
+*/
 
 void SensorManager::report(std::vector<WeatherReport> *out, std::mutex *lock_out) const{
     while (1){
